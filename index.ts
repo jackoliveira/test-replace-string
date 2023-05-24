100a9ec005c4b349ec5e358ddc3a67c0999f4f97
jackoliveira <jakku@outlook.com.br>
2023-05-24 13:10:59 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,29 +1,34 @@
-// Import stylesheets
 import './style.css';
-import assert from 'assert';
-// Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
 const STRING_BRAZE =
-  '<h>R$ 123</h> <h>R$ 123445</h> ausdhuasdhusd <h>R$ 123445</h>';
+  '<h>R$ 123</h> ausdhuasdhusd <h>R$ 123445</h> ausdhuasdhusd <h>R$ 123445</h>';
 const REGEX_TAG_H = new RegExp(/<h>(.|\n)*?<\/h>/gm);
-const REGEX_TAG_H_NO_GLOBAL = new RegExp(/<h><\/h>/);
 const STRING_BRAZE_MATCH = STRING_BRAZE.match(REGEX_TAG_H);
+const REGEX_HIGHLIGHT_TEXT = new RegExp('<h>HIGHLIGHT_TEXT</h>');
 const STRING_BRAZE_REPLACED = STRING_BRAZE.replace(
   REGEX_TAG_H,
   '<h>HIGHLIGHT_TEXT</h>'
 );
 console.info({ STRING_BRAZE_MATCH, STRING_BRAZE_REPLACED });
 
+const EXPECTED_RESULT = {
+  original: STRING_BRAZE,
+  values: '<h>R$ 123</h>;<h>R$ 123445</h>;<h>R$ 123445</h>',
+  stringWithHighlightText:
+    '<h>HIGHLIGHT_TEXT</h> ausdhuasdhusd <h>HIGHLIGHT_TEXT</h> ausdhuasdhusd <h>HIGHLIGHT_TEXT</h>',
+  final: STRING_BRAZE,
+};
+
 const parseTag = (
   stringBraze: string
 ): {
   original: string;
-  values: string;
+  values: any;
   stringWithHighlightText: string;
   final: string;
 } => {
-  const values = stringBraze.match(REGEX_TAG_H).join(';');
+  const values = stringBraze.match(REGEX_TAG_H);
   const stringWithHighlightText = stringBraze.replace(
     REGEX_TAG_H,
     '<h>HIGHLIGHT_TEXT</h>'
@@ -31,8 +36,8 @@ const parseTag = (
 
   let finalStr = stringWithHighlightText;
 
-  for (let i of values.split(';')) {
-    finalStr = finalStr.replace('<h>HIGHLIGHT_TEXT</h>', i);
+  for (let i of values) {
+    finalStr = finalStr.replace(REGEX_HIGHLIGHT_TEXT, i);
   }
 
   return {
@@ -43,45 +48,49 @@ const parseTag = (
   };
 };
 
-const expectedObj = {
-  original: STRING_BRAZE,
-  values: '<h>R$ 123</h>;<h>R$ 123445</h>;<h>R$ 123445</h>',
-  stringWithHighlightText:
-    '<h>HIGHLIGHT_TEXT</h> <h>HIGHLIGHT_TEXT</h> ausdhuasdhusd <h>HIGHLIGHT_TEXT</h>',
-  final: STRING_BRAZE,
-};
-
 const parsed: any = parseTag(STRING_BRAZE);
+appDiv.innerHTML = `<pre>${parsed.final}</pre>`;
 
-appDiv.innerHTML = `<pre>${JSON.stringify(parsed)}</pre>`;
 console.table(parsed);
+
 console.log(
   'original',
-  expectedObj.original === parsed.original,
-  expectedObj.original === parsed.original
-    ? expectedObj.original
-    : `DIFF ${parsed.original} ${expectedObj.original}`
-);
-console.log(
-  'str',
-  expectedObj.values === parsed.values,
-  expectedObj.values === parsed.values
-    ? expectedObj.values
-    : `DIFF ${parsed.values} |\n ${expectedObj.values}`
+  EXPECTED_RESULT.original === parsed.original,
+  EXPECTED_RESULT.original === parsed.original
+    ? EXPECTED_RESULT.original
+    : `DIFF ${parsed.original} ${EXPECTED_RESULT.original}`
 );
 
 console.log(
-  'strHightlight',
-  expectedObj.stringWithHighlightText === parsed.stringWithHighlightText,
-  expectedObj.stringWithHighlightText === parsed.stringWithHighlightText
-    ? expectedObj.stringWithHighlightText
-    : `DIFF ${parsed.stringWithHighlightText} |\n ${expectedObj.stringWithHighlightText}`
+  'values',
+  EXPECTED_RESULT.values === parsed.values,
+  EXPECTED_RESULT.values === parsed.values
+    ? EXPECTED_RESULT.values
+    : `DIFF ${parsed.values} |\n ${EXPECTED_RESULT.values}`
+);
+
+console.log(
+  'stringWithHighlightText',
+  EXPECTED_RESULT.stringWithHighlightText === parsed.stringWithHighlightText,
+  EXPECTED_RESULT.stringWithHighlightText === parsed.stringWithHighlightText
+    ? EXPECTED_RESULT.stringWithHighlightText
+    : `DIFF ${parsed.stringWithHighlightText} | ${EXPECTED_RESULT.stringWithHighlightText}`
 );
 
 console.log(
   'FINAL',
-  expectedObj.final === parsed.final,
-  expectedObj.final === parsed.final
-    ? expectedObj.final
-    : `DIFF ${parsed.final} |\n ${expectedObj.final}`
+  EXPECTED_RESULT.final === parsed.final,
+  EXPECTED_RESULT.final === parsed.final
+    ? EXPECTED_RESULT.final
+    : `DIFF ${parsed.final} |\n ${EXPECTED_RESULT.final}`
+);
+
+console.log(
+  parsed.stringWithHighlightText.split(' ').map((text) => {
+    if (text.includes('HIGHLIGHT_TEXT')) {
+      return `<TextHighlight>${text}</TextHighlight>`;
+    }
+
+    return `<Text>${text}</Text>`;
+  })
 );
